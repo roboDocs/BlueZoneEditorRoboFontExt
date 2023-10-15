00c96ae66044ca5c5cccafa13ad6dc886581dae0
Connor Davenport <info@connordavenport.com>
2023-10-15 15:37:15 -0400

    updating to 4.4

--- lib/Blues.py
+++ lib/Blues.py
@@ -3,11 +3,15 @@ import mojo.drawingTools as dt
 from mojo.UI import getGlyphViewDisplaySettings, setGlyphViewDisplaySettings, CurrentGlyphWindow
 from lib.tools.defaults import getDefaultColor, getDefault
 from mojo.extensions import ExtensionBundle
+from mojo.subscriber import Subscriber, registerRoboFontSubscriber
+import AppKit
+from merz import MerzPen
 
 """
 Blue Zone Editor
 by Andy Clymer, October 2018
 
+updated for RF4.4 by Connor Davenport, October 2023
 """
 
 
@@ -20,9 +24,12 @@ MARQUECOLOR = (c.redComponent(), c.greenComponent(), c.blueComponent(), c.alphaC
 c = getDefaultColor("glyphViewBluesColor")
 BLUESCOLOR = (c.redComponent(), c.greenComponent(), c.blueComponent(), c.alphaComponent())
 
-VIEWWIDTH = getDefault("glyphViewDefaultWidth")
+c = getDefaultColor("glyphViewOtherBluesColor")
+OTHERBLUESCOLOR = (c.redComponent(), c.greenComponent(), c.blueComponent(), c.alphaComponent())
 
+VIEWWIDTH = getDefault("glyphViewArtBoardHorizontalBorder") * 2
 
+KEY = "com.andyclymer.blueZoneEditor"
     
 class BlueZone(object):
     
@@ -31,27 +38,55 @@ class BlueZone(object):
     Has helper functions for selecting zone edges and moving the selection
     """
     
-    def __init__(self, startPosition, endPosition, isOther=False):
-        self.alwaysShowLabels = getDefault("com.andyclymer.blueZoneEditor.alwaysShowLabels", defaultValue=False)
+    def __init__(self, startPosition, endPosition, layer, index, isOther=False):
+        self.alwaysShowLabels = getDefault(f"{KEY}.alwaysShowLabels", defaultValue=False)
         self.startPosition = startPosition
         self.startSelected = False # and the mouse offset if it is selected
         self.endPosition = endPosition
         self.endSelected = False # and the mouse offset if it is selected
         self.isOther = isOther
-    
-    def __repr__(self):
-        if self.isOther:
-            return "<BlueZone (Other) %s %s>" % (self.startPosition, self.endPosition)
-        else: return "<BlueZone %s %s>" % (self.startPosition, self.endPosition)
+        self.layer = layer
+        self.color = BLUESCOLOR if not self.isOther else OTHERBLUESCOLOR
+        self.index = index
         
-    def moveSelection(self, delta):
-        xOffset, yOffset = delta
-        if self.startSelected:
-            self.startSelected += xOffset
-            self.startPosition -= yOffset
-        if self.endSelected:
-            self.endSelected += xOffset
-            self.endPosition -= yOffset
+        self.blue = self.layer.appendRectangleSublayer() 
+        self.arrow = self.layer.appendPathSublayer()
+    
+    
+        self.startText = self.layer.appendTextLineSublayer()
+        self.endText = self.layer.appendTextLineSublayer()
+
+        for c in [self.startText, self.endText]:
+            c.setPointSize(12)
+            c.setFillColor((0,0,0,.5))
+            c.setHorizontalAlignment("right")
+            c.setVerticalAlignment("center")
+
+    def __repr__(self):
+        o = " (Other)" if self.isOther else ""
+        return f"<BlueZone{o} {self.startPosition} {self.endPosition}>"
+        
+    def moveSelection(self, delta, isKeyed=False):
+        xOffset, yOffset = delta    
+        
+        if isKeyed:
+            if (AppKit.NSEvent.modifierFlags() & AppKit.NSAlternateKeyMask):
+                self.startSelected -= xOffset
+                self.startPosition += yOffset
+            else:
+                self.endSelected -= xOffset
+                self.endPosition += yOffset
+        else:
+            if self.startSelected and self.endSelected:
+                pass
+            else:
+                if self.startSelected:
+                    self.startSelected += xOffset
+                    self.startPosition -= yOffset
+                if self.endSelected:
+                    self.endSelected += xOffset
+                    self.endPosition -= yOffset
+                
         # Keep the start lower than the end
         if self.startPosition > self.endPosition:
             self.startPosition, self.endPosition = self.endPosition, self.startPosition
@@ -62,8 +97,18 @@ class BlueZone(object):
             elif self.endSelected and not self.startSelected:
                 self.startSelected = True
                 self.endSelected = False
+                                
         self.startPosition = int(round(self.startPosition))
         self.endPosition = int(round(self.endPosition))
+        
+        w,h = self.blue.getSize()
+        nh = (5 * round(int(self.endPosition - self.startPosition)/5)) if (AppKit.NSEvent.modifierFlags() & AppKit.NSShiftKeyMask) else int(self.endPosition - self.startPosition)
+        if self.startSelected:
+            self.blue.setPosition((-VIEWWIDTH,self.startPosition))
+        
+        self.blue.setSize((w,nh))
+        self.draw()
+        
             
     @property
     def selected(self):
@@ -79,7 +124,7 @@ class BlueZone(object):
         self.endSelected = False
         if abs(self.startPosition - point[1]) < abs(self.endPosition - point[1]):
             self.startSelected = point[0]
-        else: self.endSelected = point[0]
+        else: self.endSelected = point[0]   
         
     def distance(self, location):
         # Return the distance to either edge (whichever is closest) to the point
@@ -94,101 +139,133 @@ class BlueZone(object):
             return True
         else: return False
         
-    def draw(self, scale):
-        dt.save()
+    def draw(self):
         # Draw the zone
-        dt.fill(*BLUESCOLOR)
-        dt.stroke(None)
-        dt.rect(-1*VIEWWIDTH*scale, self.startPosition, VIEWWIDTH*2*scale, self.endPosition-self.startPosition)
+        self.blue.setPosition((-VIEWWIDTH, self.startPosition))
+        self.blue.setSize((VIEWWIDTH*4, self.endPosition-self.startPosition))
+        self.blue.setFillColor(self.color)
+        
         # See if any edges are selected
         selectedPoints = []
         if self.startSelected:
             selectedPoints += [(self.startSelected, self.startPosition)]
         if self.endSelected:
             selectedPoints += [(self.endSelected, self.endPosition)]
-        # Shade in the zone if it's selected
-        if len(selectedPoints):
-            dt.fill(*MARQUECOLOR)
-            dt.stroke(None)
-            dt.rect(-1*VIEWWIDTH*scale, self.startPosition, VIEWWIDTH*2*scale, self.endPosition-self.startPosition)
         # Draw the zone type
-        if self.isOther:
-            dt.stroke(None)
-            dt.fill(1, 1, 1, 1)
+        if not self.isOther and self.index != 0:
+            fill=(1, 1, 1, 1)
             bottom, top = self.startPosition+10, self.startPosition
-        else: 
-            dt.stroke(None)
-            dt.fill(0.2, 0.2, 0.8, 1)
+        elif self.index == 0 or self.isOther:
+            fill=(0.2, 0.2, 0.8, 1)
             bottom, top = self.endPosition-10, self.endPosition
-        dt.newPath()
-        dt.moveTo((0, top))
-        dt.lineTo((-5, bottom))
-        dt.lineTo((5, bottom))
-        dt.closePath()
-        dt.drawPath()
-        dt.restore()
-        # Draw a stroke on the selected zone edges
-        dt.fill(None)
-        dt.stroke(r=0, g=0, b=1, a=0.5)
-        dt.strokeWidth(3*scale)
-        for selectedPoint in selectedPoints:
-            dt.newPath()
-            dt.moveTo((-30, selectedPoint[1]))
-            dt.lineTo((30, selectedPoint[1]))
-            dt.drawPath()
+                
+        pen = MerzPen()
+        pen.moveTo((0, top))
+        pen.lineTo((-5, bottom))
+        pen.lineTo((5, bottom))
+        pen.closePath()
+        
+        self.arrow.setFillColor(fill)
+        self.arrow.setPath(pen.path)
+            
         # Draw the zone locations and its type
-        if len(selectedPoints) or self.alwaysShowLabels:
-            dt.fill(r=0, g=0, b=1, a=0.5)
-            dt.stroke(None)
-            positions = [self.startPosition, self.endPosition]
-            positions.sort()
-            dt.font("LucidaGrande-Bold")
-            dt.fontSize(12 * scale)
-            size = dt.textSize(str(0), align=None)
-            zoneHeight = positions[1] - positions[0]
-            if zoneHeight < 10:
-                offset = 10 - zoneHeight
-            else: offset = 0
-            dt.textBox(str(positions[0]), (-100, positions[0]-size[1]-offset, 200, size[1]), align="center")
-            dt.textBox(str(positions[1]), (-100, positions[1], 200, size[1]+(2*scale)), align="center")
-            # Write the name type
-            if len(selectedPoints):
-                if self.isOther:
-                    typeText = "OtherBlue"
-                    dt.textBox(typeText, (-100, positions[0]-(size[1]*3)-offset, 200, size[1]*2), align="center")
-                else:
-                    typeText = "BlueValue"
-                    dt.textBox(typeText, (-100, positions[1], 200, size[1]*2), align="center")
-        dt.fill(None)
-        dt.stroke(None)
+        positions = [self.startPosition, self.endPosition]
+        positions.sort()
+        size = dt.textSize(str(0), align=None)
+        zoneHeight = positions[1] - positions[0]
+        if zoneHeight < 10:
+            offset = 10 - zoneHeight
+        else: offset = 5
 
+        self.startText.setPosition((-50, positions[0]-20))
+        self.startText.setText(str(positions[0]))
+        
+        self.endText.setPosition((-50, positions[1]+20))
+        self.endText.setText(str(positions[1]))
 
+    def flip(self):
+        self.color = BLUESCOLOR if not self.isOther else OTHERBLUESCOLOR
+        self.blue.setFillColor(self.color) 
+        self.endSelected = self.startSelected = False
+        self.highlight()
+        self.draw()        
+        
+    def highlight(self):
+        r,g,b,a = self.color
+        strokeColor = (r,g,b,1)
+        
+        if self.endSelected and self.startSelected:
+            self.blue.setStrokeWidth(2)
+            self.blue.setStrokeColor(strokeColor)
+        else:
+            self.blue.setStrokeWidth(None)
+            self.blue.setStrokeColor(None)
+                                
+    def animate(self):
+        x,y =(-VIEWWIDTH, self.startPosition)
+        w,h =(VIEWWIDTH*4, self.endPosition-self.startPosition)
+        
+        pathLayer = self.layer.appendBaseSublayer(
+            position=(x, y),
+            size=(w, h))
+        
+        r,g,b,a = self.color
+        strokeColor = (r,g,b,1)
+        ani = pathLayer.appendRectangleSublayer(
+            size=(w, h),
+            fillColor=None,
+            strokeColor=strokeColor,
+            strokeWidth=2)
 
+        h *= 1.5
+        
+        d = .8
+        with ani.propertyGroup(
+                duration=d
+            ):
+            ani.setStrokeWidth(1)
+            ani.setSize((w,h))
+
+        with pathLayer.propertyGroup(
+                duration=d,
+                animationFinishedCallback=self.removePointAnimation
+            ):
+            pathLayer.setOpacity(0)
+            x,y = pathLayer.getPosition()
+            pathLayer.setPosition((x,y-(h/6)))
+            
+    def removePointAnimation(self, layer):
+        self.layer.removeSublayer(layer)
+        
+
+debug = True
 class BlueEdit(BaseEventTool):
     
     def becomeActive(self):
         # Remember the current display settings, and turn the blues off (I'll draw them myself)
-        self.previousDisplaySettings = {"Blues": getGlyphViewDisplaySettings()["Blues"], "Family Blues": getGlyphViewDisplaySettings()["Family Blues"]}
-        setGlyphViewDisplaySettings({"Blues":False, "Family Blues":False})
+        self.previousDisplaySettings = {"Blues": getGlyphViewDisplaySettings()["Blues"], "FamilyBlues": getGlyphViewDisplaySettings()["FamilyBlues"]}
+        setGlyphViewDisplaySettings({"Blues":False, "FamilyBlues":False})
         # Attributes
         self.font = CurrentFont()
         self.zones = []
         self.currentlyUpdatingInfo = False
         
-        # Start tracking the CurrentFont (if there is one)
-        self.fontChangedCallback(None)
-        # Observers
-        addObserver(self, "fontChangedCallback", "fontBecameCurrent")
-    
+        self.container = self.extensionContainer(
+            identifier=f"{KEY}.foreground",
+            location="foreground",
+            clear=True
+        )
+
+        self.fontBecameCurrent(None)
     
     def becomeInactive(self):
         # Reset the display settings
         setGlyphViewDisplaySettings(self.previousDisplaySettings)
         # Observers
-        removeObserver(self, "fontBecameCurrent")
+        self.container.clearSublayers()
+
         if not self.font == None:
             self.applyZones()
-            self.font.info.removeObserver(self, "Info.Changed")
 
 
     def getToolbarIcon(self):
@@ -201,8 +278,7 @@ class BlueEdit(BaseEventTool):
         
         
     # Observer callbacks
-        
-    def fontChangedCallback(self, info):
+    def fontBecameCurrent(self, info):
         # Forget any font-specific settings and observe on the font info
         cf = CurrentFont()
         # If there really is a new font
@@ -211,21 +287,17 @@ class BlueEdit(BaseEventTool):
             if not self.font == None:
                 # Apply the zones before switching
                 self.applyZones()
-                self.font.info.removeObserver(self, "Info.Changed")
             self.font = cf
-            if not self.font == None:
-                self.font.info.addObserver(self, "infoChanged", "Info.Changed")
         if not self.font == None:
             self.collectZones()
         
         
-    def infoChanged(self, info):
+    def currentFontInfoDidChange(self, info):
         # The font info changed
         # Cache the blue zone data because it may have changed,
         # but only if this tool is not currently editing the font info.
         if not self.currentlyUpdatingInfo:
             self.collectZones()
-    
     
     def mouseDown(self, point, count):
         yLoc = int(round(point[1]))
@@ -236,9 +308,12 @@ class BlueEdit(BaseEventTool):
                 if zone.pointInside(yLoc):
                     didFlipZone = True
                     zone.isOther = not zone.isOther
+                    zone.flip()
+                    zone.animate()
+                    
             if not didFlipZone:
                 # Double click not on a zone: add a new zone
-                self.addZone(yLoc-5, yLoc+5)
+                self.addZone(yLoc-10, yLoc+10)
         elif count == 1:
             # Single click: find the closest zone *edge* in a range and select it
             selected = self.selectClosestZoneEdge((point.x, point.y))
@@ -248,13 +323,13 @@ class BlueEdit(BaseEventTool):
                     if zone.pointInside(yLoc):
                         zone.startSelected = True
                         zone.endSelected = True
-            
+                        zone.highlight()
+
             
     def mouseDragged(self, point, delta):
         # If the mouse is dragging, move selected zones
         for zone in self.zones:
             zone.moveSelection(delta)
-    
     
     def mouseUp(self, point):
         # If any zones are selected, update the font info, they may have changed
@@ -265,23 +340,22 @@ class BlueEdit(BaseEventTool):
         if wasSelected:
             self.applyZones()
     
-    
     def keyDown(self, event):
         e = extractNSEvent(event)
         # Arrow keys to move
         moveValue = 0
         if ord(e["keyDown"]) == 63232: # Up
             if e["shiftDown"]:
-                moveValue = -10
-            else: moveValue = -1
-        elif ord(e["keyDown"]) == 63233: # Down
-            if e["shiftDown"]:
                 moveValue = 10
             else: moveValue = 1
+        elif ord(e["keyDown"]) == 63233: # Down
+            if e["shiftDown"]:
+                moveValue = -10
+            else: moveValue = -1
         if moveValue:
             for zone in self.zones:
-                zone.moveSelection((0, moveValue))
-            self.redraw()
+                if zone.selected:
+                    zone.moveSelection((0, moveValue),True)
         # Delete to remove zones
         if ord(e["keyDown"]) == 127: # Delete
             self.removeSelectedZones()
@@ -290,22 +364,7 @@ class BlueEdit(BaseEventTool):
             for zone in self.zones:
                 if zone.selected:
                     zone.isOther = not zone.isOther
-            self.redraw()
-            
-        
-    def draw(self, scale):
-        for zone in self.zones:
-            zone.draw(scale)
-                
-        
-    
-    # Helpers
-        
-        
-    def redraw(self):
-        cgw = CurrentGlyphWindow()
-        cgw.getGlyphView().refresh()
-    
+                    zone.flip()
     
     def collectZones(self):
         self.zones = []
@@ -314,10 +373,13 @@ class BlueEdit(BaseEventTool):
             if "Other" in k:
                 isOther = True
             zoneValues = getattr(self.font.info, k)
+            index = 0
             for i in range(0, len(zoneValues), 2):
-                z = BlueZone(zoneValues[i], zoneValues[i+1], isOther=isOther)
-                self.zones += [z]
-
+                z = BlueZone(zoneValues[i], zoneValues[i+1], self.container, index=index, isOther=isOther)
+                self.zones += [z]                
+                index += 1
+        for zone in self.zones:
+            zone.draw()
     
     def applyZones(self):
         for k in BLUEKEYS:
@@ -358,6 +420,7 @@ class BlueEdit(BaseEventTool):
             for zone in self.zones:
                 if not keepSelection:
                     zone.deselect()
+                    zone.highlight()
                 thisDist = zone.distance(point[1])
                 if thisDist < closestDist:
                     closestDist = thisDist
@@ -379,17 +442,16 @@ class BlueEdit(BaseEventTool):
         
         
     def addZone(self, startPos, endPos, isOther=False):
-        if isOther:
-            blueKey = "postscriptOtherBlues"
-        else: blueKey = "postscriptBlueValues"
+        blueKey = "postscriptOtherBlues" if isOther else "postscriptBlueValues"
         if not self.font == None:
             if self.countZones()[blueKey] < 7:
-                z = BlueZone(startPos, endPos, isOther=isOther)
+                z = BlueZone(startPos, endPos, self.container, index=(self.countZones()[blueKey]+1), isOther=isOther)
                 z.startSelected = True
                 z.endSelected = True
+                z.draw()
+                z.animate()
                 self.zones += [z]
                 
-    
     def removeSelectedZones(self):
         newZones = []
         for zIdx, zone in enumerate(self.zones):
@@ -397,13 +459,6 @@ class BlueEdit(BaseEventTool):
                 newZones.append(zone)
         self.zones = newZones
         self.applyZones()
-    
-    
-    def flipSelectedZone(self, index):
-        for zone in self.zones:
-            if zone.selected:
-                zone.isOther = not zone.isOther
-    
     
 
 installTool(BlueEdit())
